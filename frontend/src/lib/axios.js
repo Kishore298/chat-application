19cd453738000c7f63c3e923963255a423659c72
Kishore298 <kishore103345@gmail.com>
2025-01-03 12:43:41 +0530

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-58k4.onrender.com/api" : "/api",
   withCredentials: true,
 });
